aa54dcc26feb50e4bed363d5fa3106ee5c8429c1
Jyoti Kannaujiya <jyoti.blitzshipz@gmail.com>
2026-03-10 01:18:36 -0400

    secound

--- new.js
+++ new.js
@@ -3,3 +3,5 @@ let accountName="Jaipur"
 var accountEmail="jk12@gmail.com"
 accountPassword=123
 console.log(accountId,accountName,accountEmail,accountPassword)
+var name="jyoti"
+console.log("jyoti")
